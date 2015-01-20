91f8418b57b2cdc3232dc1908c2b6a44e737a4f6
Christian Luijten <christian@luijten.org>
2015-01-20 20:32:42 +0100

    Initial commit

---
+++ lib/hamster/versions.rb
@@ -0,0 +1,7 @@
+require "hamster/versions/version"
+
+module Hamster
+  module Versions
+    # Your code goes here...
+  end
+end
---
+++ lib/hamster/versions/version.rb
@@ -0,0 +1,5 @@
+module Hamster
+  module Versions
+    VERSION = "0.0.1"
+  end
+end
